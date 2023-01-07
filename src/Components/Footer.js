6377bdd2e045b5b831d3419e1a8d9fc67a299ea5
krishna306 <krishnakumarkeer5017@gmail.com>
2023-01-07 18:45:25 +0530

    updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+const year  = new Date().getFullYear;
 function Footer() {
   return (
-    <div className='bg-light mt-4 py-4 text-center'>Copyright {new Date().getFullYear()} @Krishna Kumar</div>
+    <div className='bg-light mt-4 py-4 text-center'>Copyright {year} @Krishna Kumar</div>
   )
 }
 
